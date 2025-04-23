f9ddcd6ca78bface57203613f81aae2eeadfd35a
Himanshu Sharma <himanshuindia2004@gmail.com>
2025-04-23 16:22:30 +0530

    Changes in index.js

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import connectDB from "./src/db/index.js";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 dotenv.config();
 
 import userRoutes from "./src/routes/user.routes.js";
@@ -11,6 +12,14 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+const corsOptions = {
+  origin: "http://localhost:5173",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
 
 app.use("/api/v1/user", userRoutes);
 app.use("/api/v1/message", messageRoutes);
