7597242e0363ba6df11f2de7f1b80e880f7843f1
Himanshu Sharma <himanshuindia2004@gmail.com>
2025-05-02 22:01:11 +0530

    Add socket.io

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,13 @@
-import express from "express";
-import connectDB from "./src/db/index.js";
+
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-dotenv.config();
-
+import connectDB from "./src/db/index.js";
 import userRoutes from "./src/routes/user.routes.js";
 import messageRoutes from "./src/routes/message.routes.js";
-const app = express();
+import { app, server } from "./socket/socket.js"; 
+import express from "express";
+dotenv.config();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -16,20 +16,19 @@ app.use(cookieParser());
 const corsOptions = {
   origin: "http://localhost:5173",
   credentials: true,
-  optionSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
 
 
 app.use("/api/v1/user", userRoutes);
 app.use("/api/v1/message", messageRoutes);
-const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello this is home to your server");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is listeing on PORT ${PORT}`);
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is listening on PORT ${PORT}`);
   connectDB();
 });
